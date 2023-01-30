b8546a7696d5fbea04cbf132464601f8335a1846
PPakSang <sanghyun-dev@naver.com>
2023-01-30 13:38:07 +0900

    Init Project

---
+++ src/main/java/com/knu/noticesender/NoticeSenderApplication.java
@@ -0,0 +1,13 @@
+package com.knu.noticesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeSenderApplication.class, args);
+    }
+
+}
