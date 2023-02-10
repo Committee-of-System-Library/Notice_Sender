1e15e7d64c6f9369328f9298fec169647072b0a2
PPakSang <sanghyun-dev@naver.com>
2023-02-10 10:25:57 +0900

    Refactor: NoticeService 주석 추가 (코드 참조)

--- src/main/java/com/knu/noticesender/notice/service/NoticeService.java
+++ src/main/java/com/knu/noticesender/notice/service/NoticeService.java
@@ -13,6 +13,14 @@ import org.springframework.stereotype.Service;
 public class NoticeService {
     private final NoticeRepository noticeRepository;
 
+    /**
+     * 공지사항 알림 상태 별 공지사항 데이터를 불러옵니다
+     *
+     * @param type: 공지사항 알림 상태
+     * @see NoticeType
+     * @see NoticeDto
+     * @return NoticeDto 로 변환된 Notice 데이터
+     */
     public List<NoticeDto> findAllByType(NoticeType type) {
         List<Notice> result = noticeRepository.findAllByType(type);
         return NoticeDto.fromList(result);
