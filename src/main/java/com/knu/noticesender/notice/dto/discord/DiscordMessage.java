d745fa6675dda42d1e6b4712f326132ca2bd5c4c
PPakSang <sanghyun-dev@naver.com>
2023-02-10 10:21:36 +0900

    Refactor: 클래스 주석 추가, 코드 컨벤션 적용 1. 디스코드 메시지 관련 참고 문서 추가 2. 메소드 선언 간 빈 라인을 삽입한다.

--- src/main/java/com/knu/noticesender/notice/dto/discord/DiscordMessage.java
+++ src/main/java/com/knu/noticesender/notice/dto/discord/DiscordMessage.java
@@ -6,11 +6,18 @@ import java.util.Map;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * Discord 에 전송할 Message 데이터를 관리하는 클래스
+ * @see <a href="https://discord.com/developers/docs/resources/webhook">디스코드 공식문서</a>
+ */
 @Data
 public class DiscordMessage {
     private Map<String, Object> message = new HashMap<>();
+
     public void setUsername(String username) {message.put("username", username);}
+
     public void setEmbeds(List<Embed> embeds) {message.put("embeds", embeds);}
+
     public void setContent(String content) {message.put("content", content);}
 
     @Data
