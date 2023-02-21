d14b00b08ced94c275a44071a096d9a703bcc937
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-02-21 14:22:23 +0900

    fix: request dto 기본 생성자 적용
    @RequestBody를 사용할 때 Jackson2HttpMessageConverter를 사용하기 떄문에 필요함

--- src/main/java/com/knu/noticesender/core/dto/Result.java
+++ src/main/java/com/knu/noticesender/core/dto/Result.java
@@ -1,10 +1,12 @@
 package com.knu.noticesender.core.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Result<T> {
     private T data;
 }
