971c9db3a807b8c22cf6384af46a4bc015557b7e
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-02-21 16:59:55 +0900

    feat: vaildation dependencies 추가

--- src/main/java/com/knu/noticesender/core/dto/Result.java
+++ src/main/java/com/knu/noticesender/core/dto/Result.java
@@ -1,12 +1,14 @@
 package com.knu.noticesender.core.dto;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
 
 @Data
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Result<T> {
+    @Valid
     private T data;
 }
