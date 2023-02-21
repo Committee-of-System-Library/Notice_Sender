9f672496cebbb5b36903f983849627a707094f96
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-02-22 03:11:37 +0900

    feat: findAllByIsRecorded, findByNotice 메서드 추가(NoticeMessageRepository)

--- src/main/java/com/knu/noticesender/notice/repository/NoticeMessageRepository.java
+++ src/main/java/com/knu/noticesender/notice/repository/NoticeMessageRepository.java
@@ -3,6 +3,15 @@ package com.knu.noticesender.notice.repository;
 import com.knu.noticesender.notice.model.Notice;
 import com.knu.noticesender.notice.model.NoticeMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface NoticeMessageRepository extends JpaRepository<NoticeMessage, Long> {
+    @Query("select nm from NoticeMessage nm join fetch nm.notice where nm.isRecorded = :isRecorded")
+    List<NoticeMessage> findAllByIsRecorded(@Param("isRecorded") boolean isRecorded);
+
+    Optional<NoticeMessage> findByNotice(Notice notice);
 }
