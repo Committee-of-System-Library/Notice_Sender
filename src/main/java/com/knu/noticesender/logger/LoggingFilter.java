a7abea4590acac077a4b28cdc9713c0fe0701db9
SangHyun-Park <51109514+PPakSang@users.noreply.github.com>
2023-03-06 14:42:02 +0900

    Logging 파일 저장, Logging Filter 추가 (#14)
    * Feat: logback-spring.xml 추가
    1. Logging Pattern 설정
    2. Logging Level 별 파일 저장 장소/기한 설정
    3. Logging Root Level 설정
    
    * Feat: LoggingFilter, Request/Response Wrapper 추가
    시스템에서 생성된 로그 외에 사용자가 요청한 HTTP Request 와 Response 를 로깅하기 위한 필터를 추가합니다.
    
    * Refactor: if-else 문 제거
    
    * Refactor: new line at end of file
    
    * Refactor: MediaTypeValue 메소드 추출 및 주석 추가
    
    * Refactor: MediaTypeValue -> MediaType 로 추출 메소드 변경

---
+++ src/main/java/com/knu/noticesender/logger/LoggingFilter.java
@@ -0,0 +1,120 @@
+package com.knu.noticesender.logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+/**
+ * HTTP request/response logging filter
+ * @see #logRequest(RequestWrapper) Request 데이터 로깅
+ * @see #logResponse(ContentCachingResponseWrapper)  Response 데이터 로깅
+ */
+@Order(1)
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+    protected static final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // multipart form data 받을 때 오류 제어
+        initRequestPartsIfMultipartFormData(request);
+
+        MDC.put("traceId", UUID.randomUUID().toString().substring(30));
+        if (isAsyncDispatch(request)) {
+            filterChain.doFilter(request, response);
+        } else {
+            doFilterWrapped(new RequestWrapper(request), new ResponseWrapper(response), filterChain);
+        }
+        MDC.clear();
+    }
+
+    /**
+     * Request Wrapper Class 사용 시 Multipart Form Data 의 getParts() 가 호출되지 않는 문제 발생
+     * 따라서 Multipart Form Data 가 전송될 경우 강제로 getParts() 를 호출해 데이터를 초기화
+     */
+    private static void initRequestPartsIfMultipartFormData(HttpServletRequest request) throws IOException, ServletException {
+        if (getMediaType(request.getContentType()).equals(MediaType.MULTIPART_FORM_DATA)) {
+            request.getParts();
+        }
+    }
+
+    /**
+     * ContentType 데이터를 바탕으로 MediaType 객체 반환
+     * Default MediaType 은 application/json
+     */
+    private static MediaType getMediaType(String contentType) {
+        return MediaType.valueOf(contentType == null ? MediaType.APPLICATION_JSON_VALUE : contentType);
+    }
+
+    protected void doFilterWrapped(RequestWrapper request, ContentCachingResponseWrapper response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            logRequest(request);
+            filterChain.doFilter(request, response);
+        } finally {
+            logResponse(response);
+            response.copyBodyToResponse();
+        }
+    }
+
+    private static void logRequest(RequestWrapper request) throws IOException {
+        String queryString = request.getQueryString();
+        log.info("Request : {} ip=[{}] uri=[{}] content-type=[{}]",
+                request.getMethod(),
+                request.getRemoteAddr(),
+                queryString == null ? request.getRequestURI() : request.getRequestURI() + queryString,
+                request.getContentType()
+        );
+
+        logPayload("Request", request.getContentType(), request.getInputStream());
+    }
+
+    private static void logResponse(ContentCachingResponseWrapper response) throws IOException {
+        log.info("Response '{}'", response.getStatus());
+        logPayload("Response", response.getContentType(), response.getContentInputStream());
+    }
+
+    private static void logPayload(String prefix, String contentType, InputStream inputStream) throws IOException {
+        boolean visible = isVisible(getMediaType(contentType));
+        if (!visible) {
+            log.info("{} Payload: Binary Content", prefix);
+            return;
+        }
+
+        byte[] content = StreamUtils.copyToByteArray(inputStream);
+        if (content.length > 0) {
+            String contentString = new String(content).replace("\n", "").replaceAll(" ", "");
+            log.info("{} Payload: {}", prefix, contentString);
+        }
+    }
+
+    private static boolean isVisible(MediaType mediaType) {
+        final List<MediaType> VISIBLE_TYPES = Arrays.asList(
+                MediaType.valueOf("text/*"),
+                MediaType.APPLICATION_FORM_URLENCODED,
+                MediaType.APPLICATION_JSON,
+                MediaType.APPLICATION_XML,
+                MediaType.valueOf("application/*+json"),
+                MediaType.valueOf("application/*+xml"),
+                MediaType.MULTIPART_FORM_DATA
+        );
+
+        return VISIBLE_TYPES.stream()
+                .anyMatch(visibleType -> visibleType.includes(mediaType));
+    }
+}
