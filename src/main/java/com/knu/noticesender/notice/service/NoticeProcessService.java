48309ecac6bf79ff6aad5ca201738ec0fee60753
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-03-09 17:20:15 +0900

    fix: 안 쓰는 Import 삭제

--- src/main/java/com/knu/noticesender/notice/service/NoticeProcessService.java
+++ src/main/java/com/knu/noticesender/notice/service/NoticeProcessService.java
@@ -6,7 +6,6 @@ import com.knu.noticesender.notice.dto.NoticeSaveReqDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
