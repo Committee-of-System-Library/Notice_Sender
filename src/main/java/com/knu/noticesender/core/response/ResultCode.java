d0713c43147233f359d30096911f833a53a2f5fd
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-03-27 02:53:39 +0900

    fix:Result code 양식 수정
    status(3자리)_숫자(3자리)로 수정함
    같은 status에 대해서 999개 까지 에러를 추가할 수 있음

--- src/main/java/com/knu/noticesender/core/response/ResultCode.java
+++ src/main/java/com/knu/noticesender/core/response/ResultCode.java
@@ -6,8 +6,12 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ResultCode {
-    SUCCESS(HttpStatus.OK, 200, "success"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "internal server error");
+
+    /*
+     status(3자리)_숫자(3자리)
+     */
+    SUCCESS(HttpStatus.OK, 200_001, "success"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500_001, "internal server error");
     private HttpStatus httpStatus;
     private int code;
     private String message;
